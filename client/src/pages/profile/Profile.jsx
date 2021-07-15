b591c29652c28dbe5ba4a08ae7935d4994d2af32
Masayuki <soy2.san@gmail.com>
2021-07-15 15:27:00 +0900

    profile page

---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import './profile.css';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Topbar from '../../components/topbar/Topbar';
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img className="profileCoverImg" src="assets/post/post3.jpg" alt="" />
+              <img className="profileUserImg" src="assets/post/post7.jpg" alt="" />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">some name</h4>
+              <span className="profileInfoDesc">Hello </span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
