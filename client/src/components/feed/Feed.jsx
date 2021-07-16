ee6a42aa41a63a11aac7f019bbdd961eaa33e1d4
Masayuki <soy2.san@gmail.com>
2021-07-17 01:22:46 +0900

    hooks and display dynamic

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,15 +1,28 @@
 import './feed.css';
 import Share from '../share/Share';
 import Post from '../post/Post';
-import { Posts } from '../../dummyData';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Feed() {
+export default function Feed({username}) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/post/profile/" + username)
+        : await axios.get("/post/timeline/60f037eef61919a9e4a34862");
+      setPosts(res.data);
+    }
+    fetchPosts();
+  }, [username]);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map(p => (
-          <Post key={p.id} post={p} />
+        {posts.map(p => (
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,10 +3,20 @@ import Feed from '../../components/feed/Feed';
 import Rightbar from '../../components/rightbar/Rightbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Topbar from '../../components/topbar/Topbar';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(`/users?username=happy`);
+      setUser(res.data);
+    }
+    fetchPosts();
+  }, []);
   return (
     <>
       <Topbar />
@@ -15,16 +25,22 @@ export default function Profile() {
         <div className="profileRight">
           <div className="profileRightTop">
             <div className="profileCover">
-              <img className="profileCoverImg" src={`${PF}post/post3.jpg`} alt="" />
-              <img className="profileUserImg" src={`${PF}people/p7.jpg`} alt="" />
+              <img className="profileCoverImg"
+                src={`${PF}post/post3.jpg`}
+                alt=""
+              />
+              <img className="profileUserImg"
+                src={`${PF}people/p7.jpg`}
+                alt=""
+              />
             </div>
             <div className="profileInfo">
-              <h4 className="profileInfoName">some name</h4>
-              <span className="profileInfoDesc">Hello </span>
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed />
+            <Feed username="happy" />
             <Rightbar profile />
           </div>
         </div>
