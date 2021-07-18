9dda42c8f1a108c37570085794af11607f5c4a6c
Masayuki <soy2.san@gmail.com>
2021-07-19 01:32:43 +0900

    add multer and path

--- api/index.js
+++ api/index.js
@@ -7,20 +7,41 @@ const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/post');
+const multer = require('multer');
+const path = require('path');
 
 dotenv.config();
 
 mongoose.connect(
   process.env.MONGODB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log(err));
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => console.log('Connected to MongoDB')
+);
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully")
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
